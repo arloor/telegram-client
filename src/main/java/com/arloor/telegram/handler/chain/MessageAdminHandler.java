d4f53518120a3f65b1f2a1e0b86b2d795a029ca3
arloor <admin@arloor.com>
2020-08-24 23:23:59 +0800

    啊哈哈哈

--- src/main/java/com/arloor/telegram/handler/chain/MessageAdminHandler.java
+++ src/main/java/com/arloor/telegram/handler/chain/MessageAdminHandler.java
@@ -29,54 +29,47 @@ public class MessageAdminHandler extends BaseHandler<TdApi.UpdateNewMessage> {
             chatName = targetChat.title;
         }
 
-        if (myGroups == null ||  !myGroups.contains(String.valueOf(chatId))) {
-            return false;
-        } else {
-            // 本群组的所有消息类型都日志记录
-//            logger.info(newLine + "(" + sender + ")@" + chatName + " " + senderID + "@" + chatId + newLine + message);
-            if (message.message.content instanceof TdApi.MessageChatAddMembers && message.message.date > bootUnixTime) {
-                int[] memberUserIds = ((TdApi.MessageChatAddMembers) message.message.content).memberUserIds;
-                String finalChatName = chatName;
-                Arrays.stream(memberUserIds).forEach(newMemberId -> {
+        if (message.message.content instanceof TdApi.MessageChatAddMembers && message.message.date > bootUnixTime) {
+            int[] memberUserIds = ((TdApi.MessageChatAddMembers) message.message.content).memberUserIds;
+            String finalChatName = chatName;
+            Arrays.stream(memberUserIds).forEach(newMemberId -> {
 
-                    String newMemberStr = String.valueOf(newMemberId);
-                    TdApi.User newMemberUser = getOrQueryUser(newMemberId);
-                    if (Objects.nonNull(newMemberUser)) {
-                        newMemberStr = newMemberUser.firstName + " " + (newMemberUser.lastName.length() > 0 ? newMemberUser.lastName : "");
-                    }
+                String newMemberStr = String.valueOf(newMemberId);
+                TdApi.User newMemberUser = getOrQueryUser(newMemberId);
+                if (Objects.nonNull(newMemberUser)) {
+                    newMemberStr = newMemberUser.firstName + " " + (newMemberUser.lastName.length() > 0 ? newMemberUser.lastName : "");
+                }
 
-                    String msg = String.format("欢迎[新朋友](tg://user?id=%s)来到本群组！", newMemberId) + newLine;
+                String msg = String.format("欢迎[新朋友](tg://user?id=%s)来到本群组！", newMemberId) + newLine;
 
-                    TdApi.ReplyMarkupInlineKeyboard replyMarkup = null;
-                    if (Telegram.me != null && Telegram.me.type instanceof TdApi.UserTypeBot) {//如果是bot，则增加防bot设置
-                        String adminPass = JSONObject.toJSONString(new Op(Op.ADMIN_PASS, newMemberId, chatId));
-                        String nobot = JSONObject.toJSONString(new Op(Op.NOBOT, newMemberId, chatId));
-                        TdApi.InlineKeyboardButton[] rowBlogAndGithub = {new TdApi.InlineKeyboardButton("arloor.com", new TdApi.InlineKeyboardButtonTypeUrl("http://arloor.com")), new TdApi.InlineKeyboardButton("Github", new TdApi.InlineKeyboardButtonTypeUrl("https://github.com/arloor"))};
-                        TdApi.InlineKeyboardButton[] notBotRow = {new TdApi.InlineKeyboardButton("我不是机器人", new TdApi.InlineKeyboardButtonTypeCallback(nobot.getBytes()))};
-                        TdApi.InlineKeyboardButton[] adminPassRow = {new TdApi.InlineKeyboardButton("PASS[管理员]", new TdApi.InlineKeyboardButtonTypeCallback(adminPass.getBytes()))};
-                        replyMarkup = new TdApi.ReplyMarkupInlineKeyboard(new TdApi.InlineKeyboardButton[][]{notBotRow});
-                        logger.info(String.format("封禁新加群的%s@%s %s@%s", newMemberStr, finalChatName, newMemberId, chatId)); //打印文本
-                        Telegram.client.send(new TdApi.SetChatMemberStatus(chatId, newMemberId, new TdApi.ChatMemberStatusRestricted(true, 0, new TdApi.ChatPermissions(false, false, false, false, false, false, false, false))), defaultHandler);
-                        msg += "请点击*我不是机器人*获取发言权限";
+                TdApi.ReplyMarkupInlineKeyboard replyMarkup = null;
+                if (Telegram.me != null && Telegram.me.type instanceof TdApi.UserTypeBot) {//如果是bot，则增加防bot设置
+                    String adminPass = JSONObject.toJSONString(new Op(Op.ADMIN_PASS, newMemberId, chatId));
+                    String nobot = JSONObject.toJSONString(new Op(Op.NOBOT, newMemberId, chatId));
+                    TdApi.InlineKeyboardButton[] rowBlogAndGithub = {new TdApi.InlineKeyboardButton("arloor.com", new TdApi.InlineKeyboardButtonTypeUrl("http://arloor.com")), new TdApi.InlineKeyboardButton("Github", new TdApi.InlineKeyboardButtonTypeUrl("https://github.com/arloor"))};
+                    TdApi.InlineKeyboardButton[] notBotRow = {new TdApi.InlineKeyboardButton("我不是机器人", new TdApi.InlineKeyboardButtonTypeCallback(nobot.getBytes()))};
+                    TdApi.InlineKeyboardButton[] adminPassRow = {new TdApi.InlineKeyboardButton("PASS[管理员]", new TdApi.InlineKeyboardButtonTypeCallback(adminPass.getBytes()))};
+                    replyMarkup = new TdApi.ReplyMarkupInlineKeyboard(new TdApi.InlineKeyboardButton[][]{notBotRow});
+                    logger.info(String.format("封禁新加群的%s@%s %s@%s", newMemberStr, finalChatName, newMemberId, chatId)); //打印文本
+                    Telegram.client.send(new TdApi.SetChatMemberStatus(chatId, newMemberId, new TdApi.ChatMemberStatusRestricted(true, 0, new TdApi.ChatPermissions(false, false, false, false, false, false, false, false))), defaultHandler);
+                    msg += "请点击*我不是机器人*获取发言权限";
+                } else {
+                    msg += "博客地址：http://arloor.com" + newLine
+                            + "Github：https://github.com/arloor";
+                }
+
+                TdApi.ReplyMarkupInlineKeyboard finalReplyMarkup = replyMarkup;
+                String finalMsg = msg;
+                Telegram.client.send(new TdApi.ParseTextEntities(msg, new TdApi.TextParseModeMarkdown(2)), (formattedText) -> {
+                    if (formattedText instanceof TdApi.FormattedText) {
+                        TdApi.InputMessageContent content = new TdApi.InputMessageText((TdApi.FormattedText) formattedText, false, true);
+                        Telegram.client.send(new TdApi.SendMessage(chatId, message.message.id, null, finalReplyMarkup, content), defaultHandler);
                     } else {
-                        msg += "博客地址：http://arloor.com" + newLine
-                                + "Github：https://github.com/arloor";
+                        TdApi.InputMessageContent content = new TdApi.InputMessageText(new TdApi.FormattedText(finalMsg, null), false, true);
+                        Telegram.client.send(new TdApi.SendMessage(chatId, message.message.id, null, finalReplyMarkup, content), defaultHandler);
                     }
-
-                    TdApi.ReplyMarkupInlineKeyboard finalReplyMarkup = replyMarkup;
-                    String finalMsg = msg;
-                    Telegram.client.send(new TdApi.ParseTextEntities(msg, new TdApi.TextParseModeMarkdown(2)), (formattedText) -> {
-                        if (formattedText instanceof TdApi.FormattedText) {
-                            TdApi.InputMessageContent content = new TdApi.InputMessageText((TdApi.FormattedText) formattedText, false, true);
-                            Telegram.client.send(new TdApi.SendMessage(chatId, message.message.id, null, finalReplyMarkup, content), defaultHandler);
-                        } else {
-                            TdApi.InputMessageContent content = new TdApi.InputMessageText(new TdApi.FormattedText(finalMsg, null), false, true);
-                            Telegram.client.send(new TdApi.SendMessage(chatId, message.message.id, null, finalReplyMarkup, content), defaultHandler);
-                        }
-                    });
                 });
-
-            }
+            });
         }
         return false;
     }
