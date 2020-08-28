750d3c05f9d810f4f23b951d50b8974a2b1cda87
arloor <admin@arloor.com>
2020-08-28 20:46:45 +0800

    delete

--- deploy.sh
+++ deploy.sh
@@ -28,10 +28,6 @@ cat > /opt/tg/telegram.properties <<EOF
 apiId=861784
 apiHash=dbaf939227b6ff24f0a0521e329c91e6
 author=878823128
-# 测试群组
-#mygroup=-1001248492840
-# arloor博客
-mygroup=-1001334979774,-1001446471776
 EOF
 systemctl daemon-reload
 systemctl enable tg
